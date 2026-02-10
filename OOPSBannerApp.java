8bfbe21a7e87f3e4ef043eec14d78f452282d98d
Aditya Malik <connectadityamalik@gmail.com>
2026-02-10 14:16:43 +0530

    Coded UC2 to Render OOPS as Banner

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,5 +1,9 @@
 public class OOPSBannerApp {
     public static void main(String[] args) {
-        System.out.println("OOPS");
+        System.out.println(" OOOO   PPPP   SSSS ");
+        System.out.println("O    O  P   P  S    ");
+        System.out.println("O    O  PPPP    SSS ");
+        System.out.println("O    O  P          S");
+        System.out.println(" OOOO   P      SSSS ");
     }
 }
