0ad43297af8e0333f9a70c5210633f0fb7ff6778
Aditya Malik <connectadityamalik@gmail.com>
2026-02-10 19:57:51 +0530

    Update OOPSBannerApp.java

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,16 +1,5 @@
-/**
- * OOPSBannerApp displays the text "OOPS" to the console.
- * This is the foundational Use Case (UC1) for the project.
- * * @author Aditya Malik
- * @version 1.0
- */
 public class OOPSBannerApp {
-    /**
-     * Entry point of the application.
-     * @param args Command line arguments
-     */
     public static void main(String[] args) {
-        // UC1 Requirement: Print exactly "OOPS" without quotes [cite: 260, 262]
-        System.out.println("OOPS");
+                System.out.println("OOPS");
     }
 }
