5986705c3c41a397781a3b86eab040c83693ec90
Aditya Malik <connectadityamalik@gmail.com>
2026-02-10 14:43:42 +0530

    Coded UC1 to Print OOPS to Console

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,5 +1,16 @@
+/**
+ * OOPSBannerApp displays the text "OOPS" to the console.
+ * This is Use Case 1 (UC1) of the project.
+ * @author Aditya Malik
+ * @version 1.0
+ */
 public class OOPSBannerApp {
+    /**
+     * Entry point of the application.
+     * @param args Command line arguments
+     */
     public static void main(String[] args) {
+        // Goal: Display the literal text "OOPS" [cite: 260]
         System.out.println("OOPS");
     }
 }
