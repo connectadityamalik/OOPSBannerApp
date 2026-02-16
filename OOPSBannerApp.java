da05470b22c3b18a27c1c0cfbee4239a55266779
Aditya Malik <connectadityamalik@gmail.com>
2026-02-16 13:05:01 +0530

    Update OOPSBannerApp.java

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,9 +1,3 @@
-/**
- * OOPSBannerApp UC3 OOPS Banner Application (Use Case 3)
- * This class improves the banner display by using String.join() for efficiency.
- * @author Aditya Malik
- * @version 3.0
- */
 public class OOPSBannerApp {
     public static void main(String[] args) {
         // UC3 Goal: Use String.join() with an empty delimiter "" [cite: 158, 161]
