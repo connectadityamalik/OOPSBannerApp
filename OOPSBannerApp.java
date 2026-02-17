f75f127b688a2b92da2190cc856b5afcbaa3e7b9
Aditya Malik <connectadityamalik@gmail.com>
2026-02-17 13:52:07 +0530

    Refactored UC6 to use Static Helper Methods

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,21 +1,56 @@
+
 public class OOPSBannerApp {
 
+    // Main method to run the banner display
     public static void main(String[] args) {
-        // 1. Create a String array to store all 7 lines of the banner
-        String[] banner = new String[7];
+        // 1. Get the patterns for each letter using helper methods
+        String[] oPattern = getOPattern();
+        String[] pPattern = getPPattern();
+        String[] sPattern = getSPattern();
 
-        // 2. Populate the array using String.join to construct each line
-        banner[0] = String.join("", "  *** ", "   *** ", " ***** ", "  **** ");
-        banner[1] = String.join("", " * * ", "  * * ", " * * ", " * ");
-        banner[2] = String.join("", " * * ", "  * * ", " * * ", " * ");
-        banner[3] = String.join("", " * * ", "  * * ", " ***** ", "  *** ");
-        banner[4] = String.join("", " * * ", "  * * ", " * ", "     * ");
-        banner[5] = String.join("", " * * ", "  * * ", " * ", "     * ");
-        banner[6] = String.join("", "  *** ", "   *** ", " * ", " **** ");
-
-        // 3. Use an Enhanced For-Loop to iterate through the array and print each line
-        for (String line : banner) {
-            System.out.println(line);
+        // 2. Loop through the arrays and print the combined banner
+        // We assume all patterns have the same height (7 lines)
+        for (int i = 0; i < oPattern.length; i++) {
+            System.out.println(oPattern[i] + oPattern[i] + pPattern[i] + sPattern[i]);
         }
+    }
+
+    // Helper Method for Letter O
+    public static String[] getOPattern() {
+        return new String[] {
+            "  *** ",
+            " * * ",
+            " * * ",
+            " * * ",
+            " * * ",
+            " * * ",
+            "  *** "
+        };
+    }
+
+    // Helper Method for Letter P
+    public static String[] getPPattern() {
+        return new String[] {
+            " PPPP   ",
+            " P   P  ",
+            " P   P  ",
+            " PPPP   ",
+            " P      ",
+            " P      ",
+            " P      "
+        };
+    }
+
+    // Helper Method for Letter S
+    public static String[] getSPattern() {
+        return new String[] {
+            "  SSSS  ",
+            " S      ",
+            " S      ",
+            "  SSS   ",
+            "     S  ",
+            "     S  ",
+            " SSSS   "
+        };
     }
 }
